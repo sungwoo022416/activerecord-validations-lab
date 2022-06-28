df616b6a885730d7b6147fffa3c28cf1a691ddcb
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-06-28 10:22:17 -0700

    commit tree a8c8542a80c6bda9bd05d8325326a250680b62cb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate  :clickbait
+
+    CLICK_BAITS = [
+        /Won't Believe/,
+        /Secret/,
+        /Top [0-9]/,
+        /Guess/
+    ]
+    def clickbait
+        if CLICK_BAITS.none? {|thing| thing.match title}
+            errors.add(:title, "It's a clickbait!")
+        end
+    end
 end
